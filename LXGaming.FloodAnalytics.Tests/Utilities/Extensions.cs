6b0bfbc3d2d2ddf4f54809b5ab7bcd2aa3925339
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2024-01-10 19:57:16 +1300

    Prevent duplicate service registration

--- LXGaming.FloodAnalytics.Tests/Utilities/Extensions.cs
+++ LXGaming.FloodAnalytics.Tests/Utilities/Extensions.cs
@@ -2,6 +2,7 @@
 using LXGaming.Configuration;
 using LXGaming.Configuration.Hosting;
 using LXGaming.FloodAnalytics.Configuration;
+using LXGaming.FloodAnalytics.Services.Web;
 using LXGaming.FloodAnalytics.Tests.Configuration;
 using LXGaming.FloodAnalytics.Tests.Services.Quartz;
 using LXGaming.FloodAnalytics.Tests.Services.Web;
@@ -13,16 +14,28 @@ namespace LXGaming.FloodAnalytics.Tests.Utilities;
 public static class Extensions {
 
     public static IServiceCollection AddConfiguration(this IServiceCollection services) {
+        if (services.Any(descriptor => descriptor.ServiceType == typeof(IConfiguration))) {
+            throw new InvalidOperationException("Configuration is already registered");
+        }
+
         var configuration = new DefaultConfiguration();
         configuration.Register(nameof(Config), new TestProvider<Config>());
         return services.AddConfiguration(configuration);
     }
 
     public static IServiceCollection AddSchedulerFactory(this IServiceCollection services) {
+        if (services.Any(descriptor => descriptor.ServiceType == typeof(ISchedulerFactory))) {
+            throw new InvalidOperationException("SchedulerFactory is already registered");
+        }
+
         return services.AddSingleton<ISchedulerFactory, TestSchedulerFactory>();
     }
 
     public static IServiceCollection AddWebService(this IServiceCollection services) {
+        if (services.Any(descriptor => descriptor.ServiceType == typeof(WebService))) {
+            throw new InvalidOperationException("WebService is already registered");
+        }
+
         return services
             .AddConfiguration()
             .AddLogging()
