700c62718ee1c323cd0452eaebae40af9603ae09
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2025-07-25 22:59:44 +1200

    Update LXGaming.Common

--- LXGaming.FloodAnalytics/Utilities/Constants.cs
+++ LXGaming.FloodAnalytics/Utilities/Constants.cs
@@ -11,7 +11,7 @@ public static class Constants {
         public const string Authors = "Alex Thomson";
         public const string Website = "https://lxgaming.me/";
 
-        public static readonly string Version = AssemblyUtils.GetVersion(Assembly.GetExecutingAssembly(), "Unknown");
+        public static readonly string Version = AssemblyUtils.GetVersion(Assembly.GetExecutingAssembly()) ?? "Unknown";
         public static readonly string UserAgent = $"{Name}/{Version} (+{Website})";
     }
 }
